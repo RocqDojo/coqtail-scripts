6bbaf5bd770a618b46309d8b3099bb3693d5df77
hehelego <hahalegao@gmail.com>
2025-03-31 14:52:39 +0800

    record output in a log file

--- run-with-root.sh
+++ run-with-root.sh
@@ -5,5 +5,6 @@ echo "working on $(find $1 -name '*.v' | wc -l) files ..."
 echo "wait 3 seconds before starting"
 sleep 3
 
-find $1 -name '*.v' | xargs -P 20 -I {} python runner.py {}
+find $1 -name '*.v' | xargs -P 20 -I {} python runner.py {} | tee "$(date -Iminutes).log"
+
 
